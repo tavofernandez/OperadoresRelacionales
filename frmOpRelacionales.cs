6d8ba8a71ce5c9109e1361fd43975a081679a852
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-11 22:33:40 -0500

    Se agrega el proyecto

---
+++ frmOpRelacionales.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OperadoresRelacionales
+{
+    public partial class frmOpRelacionales : Form
+    {
+        public frmOpRelacionales()
+        {
+            InitializeComponent();
+        }
+    }
+}
