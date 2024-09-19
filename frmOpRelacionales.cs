6316715eb893861422a6e45954c8a5ed9b61ad8a
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-18 21:50:53 -0500

    Llamamos al metodo correspondiente al boton menor

--- frmOpRelacionales.cs
+++ frmOpRelacionales.cs
@@ -60,7 +60,7 @@ namespace OperadoresRelacionales
 
         private void btnmenor_Click(object sender, EventArgs e)
         {
-            
+            Menorque();
         }
 
         private void Menorque()
